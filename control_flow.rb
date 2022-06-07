1d04149109c2386506c6646e6e6a24b60ed46714
Brigham <brig.johnson@gmail.com>
2022-06-06 21:28:43 -0400

    commit tree ff29147db6e2a7311a4ea5151f859872119b59f2

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,54 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+  if (username == 'admin' || username == 'ADMIN') && (password == '12345')
+      return 'Access granted'
+  else
+    return 'Access denied'
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  #return
+  case
+  when temperature < 40
+    "It's brisk out there!"
+  when temperature < 65
+    "It's a little chilly out there!"
+  when temperature <= 85
+    "It's perfect out there!"
+  else
+    "It's too dang hot out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  response = ''
+
+  response = 'Fizz' if num % 3 == 0
+  response += 'Buzz' if num % 5 == 0
+
+  if response.length > 0
+    ;; # null  # return response
+  else
+    response = num # .to_s # no, it wants num, not str
+  end
+
+  return response
 end
+#fizzbuzz(11)
 
 def calculator(operation, num1, num2)
-  # your code here
+  #answer = 
+  case operation
+  when '+' then num1 + num2
+  when '-' then num1 - num2
+  when '*' then num1 * num2
+  when '/' then num1 / num2
+  #when '/' && (num2 != 0) then num1 / num2
+  else
+    puts 'Invalid operation!'
+    nil
+  end
+  #binding.pry
 end
-
